ca166a2047933e936c41e0e1c5880113d7835dbc
Harshshah28 <imharshshah28786@gmail.com>
2023-04-12 17:46:12 +0530

    jokes

---
+++ src/test/java/com/example/jokesapk/JokesapkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jokesapk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesapkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
